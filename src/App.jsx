d1994d0b55b141e32bdf3a7217f1628b32c8e3e2
unknown <sripranathi.indupalli.s72@kalvium.community>
2025-03-04 14:37:07 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ServiceCard from './components/ServiceCard'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <ServiceCard/>
+    <h3>Services Available:</h3>
+    <p><strong> 1. Hands-on experience </strong></p>
+    <p><strong> 2. Trendy skills in current market</strong></p>
+    </>
+  )
+}
+export default App
---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ServiceCard = () => {
+    return (
+        <div>
+            <h1>Web Development</h1>
+            <h2>We call out passionate people to join our course and ace their skills in Web Development!</h2>
+        </div>
+    )
+}
+
+export default ServiceCard;
